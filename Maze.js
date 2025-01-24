7752a49326c703a58ec9c92d17f2ed1447848241
Roberttanase <RobertTanase7999@gmail.com>
2025-01-24 08:01:02 -0500

    Initial Maze.js Program

---
+++ Maze.js
@@ -0,0 +1,75 @@
+//Team Members: Robert Tanase and Alex Jones
+//Date: 1.24.2025
+//Program: Maze
+//Hardware: Bolt #SB-F28B
+
+//begin program
+async function startProgram() {
+
+//Start and Say Start
+//Bolt will say Start across the LED's and Say start out loud
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+
+
+
+//Green Checkpoint
+//LED's turn Green
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+
+
+
+//Sound #1 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+
+
+//Red Checkpoint
+//LED's turn Red
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+
+
+
+//Sound #2 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+
+//Purple Checkpoint
+//LED's turn Purple
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+
+
+//Sound #3 Checkpoint
+//Bolt makes sound of choice - Update Sound
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+
+//Blue Checkpoint
+//LED's turn Blue
+//Driver: Robert Tanase
+//Navigator: Alex Jones
+
+
+
+
+//Finished and say finished
+//Bolt will say Finished across the LED's and Say Finished out loud
+//Driver: Alex Jones
+//Navigator: Robert Tanase
+
+
+
+//End Program
+}
