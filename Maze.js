e56a319f3d10e098689376c1780dab425bfff3ab
Roberttanase <RobertTanase7999@gmail.com>
2025-01-29 07:52:28 -0500

    Checkpoint #2

--- Maze.js
+++ Maze.js
@@ -11,42 +11,59 @@ async function startProgram() {
 //Driver: Alex Jones
 //Navigator: Robert Tanase
 
-	await speak("Start", true);
-	await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true);
+	//await speak("Start", true);
+	//await scrollMatrixText('start', { r: 66, g: 56, b: 255 }, 30, true);
 
 //Green Checkpoint
 //LED's turn Green
 //Driver: Robert Tanase
 //Navigator: Alex Jones
 	await delay(1);
-	await roll((getHeading() + 0), 25, 7.5);
+	await roll((getHeading() + 0), 25, 7);
 	await delay(1);
 	setMainLed({ r: 0, g: 255, b: 0 });
+	await delay(1);
 //Sound #1 Checkpoint
 //Bolt makes sound of choice - Update Sound
 //Driver: Alex Jones
 //Navigator: Robert Tanase
 
  	await spin(90, 1);
+	await delay(1);
 	await roll((getHeading() + 0), 25, 4);
+	await delay(1);
 	await spin(90,1);
-	await roll((getHeading() + 0), 25, 3);
+	await delay(1);
+	await roll((getHeading() + 0), 25, 2.5);
+	await delay(1);
 	await Sound.Game.Coin.play(true);
+	await delay(1);
 //Red Checkpoint
 //LED's turn Red
 //Driver: Robert Tanase
 //Navigator: Alex Jones
- 	await spin(45, 1);
-	await roll((getHeading() + 0), 25, 3.5);
+ 	await spin(40, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 25, 2.5);
+	await delay(1);
 	setMainLed({ r: 255, g: 0, b: 0 });
+	await delay(1);
 
 
 //Sound #2 Checkpoint
 //Bolt makes sound of choice - Update Sound
 //Driver: Alex Jones
 //Navigator: Robert Tanase
-
-
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 25, 2);
+	await delay(1);
+	await spin(-90, 1);
+	await delay(1);
+	await roll((getHeading() + 0), 25, 2.8);
+	await delay(1);
+	await Sound.Animal.Cat.play(true);
+	await delay(1);
 //Purple Checkpoint
 //LED's turn Purple
 //Driver: Robert Tanase
